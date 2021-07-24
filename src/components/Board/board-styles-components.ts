e5372a8ef0c48a30899c232e77f5acd85f450db5
Ngassaki <cardorelngassaki@gmail.com>
2021-07-24 17:40:25 +0300

    css

--- src/components/Board/board-styles-components.ts
+++ src/components/Board/board-styles-components.ts
@@ -4,7 +4,7 @@ export const BoardRow = styled.div
     `
   display: grid;
   grid: 120px 120px 120px / 120px 120px 120px;
-  background-color: var(--bg-color);
+  background-color:#a454bc;
   gap: 5px;
   width: calc(3 * 120px + 2 * 5px);
   height: calc(3 * 120px + 2 * 5px);
